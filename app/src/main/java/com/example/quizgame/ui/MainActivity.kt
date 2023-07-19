b7f4fd4ff2435c7f88a6c626104d60915e8b8edb
Marcos <mramosmarcos14@gmail.com>
2023-07-18 22:37:58 -0300

    feat: Create application class and start main screen with navigation.

--- app/src/main/java/com/example/quizgame/ui/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.quizgame.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quizgame.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
